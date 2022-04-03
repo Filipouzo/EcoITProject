ad825c5c3633e434d0b893cb3d582b9deacc8282
Philippe ARNAL <philiparnal@gmail.com>
2022-04-03 14:34:43 +0200

    Test Revert Git

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,6 +16,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'integer')]
     private $id;
 
+
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $email;
 
