0a6503f0046a98d9e0c98c8f13e7e58be9a87546
Philippe ARNAL <philiparnal@gmail.com>
2022-07-25 18:58:05 +0200

    y

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Nat\DeployBundle\NatDeployBundle::class => ['all' => true],
 ];
